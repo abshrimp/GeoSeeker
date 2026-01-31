bd5a41a76883c881f350ae121c83a29dc7e82ec7
abshrimp <abshrimps@gmail.com>
2026-01-31 11:15:47 +0900

    edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,44 +2,50 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import fs from 'fs';
-import path from 'path';
 
-export default defineConfig({
-    base: '/',
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            includeAssets: ['/favicon.ico', 'assets/apple-touch-icon.png'],
-            injectRegister: 'auto',
-            manifest: {
-                name: 'GeoSeeker',
-                short_name: 'GeoSeeker',
-                description: 'Geography Guessing Game',
-                theme_color: '#ffffff',
-                start_url: '/game',
-                icons: [
-                    {
-                        src: 'assets/192_img.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'assets/512_img.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    }
-                ]
-            }
-        })
-    ],
-    optimizeDeps: {
-        exclude: ['lucide-react'],
-    },
-    server: {
-        https: {
-            cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
-            key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+export default defineConfig(({ command }) => {
+    const isDev = command === "serve"
+
+    return {
+        base: '/',
+        plugins: [
+            react(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                includeAssets: ['/favicon.ico', 'assets/apple-touch-icon.png'],
+                injectRegister: 'auto',
+                manifest: {
+                    name: 'GeoSeeker',
+                    short_name: 'GeoSeeker',
+                    description: 'Geography Guessing Game',
+                    theme_color: '#ffffff',
+                    start_url: '/game',
+                    icons: [
+                        {
+                            src: 'assets/192_img.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        },
+                        {
+                            src: 'assets/512_img.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        }
+                    ]
+                }
+            })
+        ],
+        optimizeDeps: {
+            exclude: ['lucide-react'],
         },
-    },
+        
+        server: isDev
+        ? {
+            https: {
+                key: fs.readFileSync("localhost.key"),
+                cert: fs.readFileSync("localhost.crt"),
+            },
+            }
+        : undefined,
+    };
 });
